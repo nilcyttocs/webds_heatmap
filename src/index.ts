e4ce3bc1d4ec436b1ddad04080723eedd64920d1
Scott Lin <scott.lin@tw.synaptics.com>
2022-06-15 00:03:33 -0700

    Switch to using WebDSWidget

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,11 @@ import {
   JupyterFrontEndPlugin
 } from "@jupyterlab/application";
 
-import { MainAreaWidget, WidgetTracker } from "@jupyterlab/apputils";
+import { WidgetTracker } from "@jupyterlab/apputils";
 
 import { ILauncher } from "@jupyterlab/launcher";
 
-import { WebDSService } from "@webds/service";
+import { WebDSService, WebDSWidget } from "@webds/service";
 
 import { heatmapIcon } from "./icons";
 
@@ -29,7 +29,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
   ) => {
     console.log("JupyterLab extension @webds/heatmap is activated!");
 
-    let widget: MainAreaWidget;
+    let widget: WebDSWidget;
     const { commands, shell } = app;
     const command: string = "webds_heatmap:open";
     commands.addCommand(command, {
@@ -41,7 +41,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
       execute: () => {
         if (!widget || widget.isDisposed) {
           const content = new HeatmapWidget(service);
-          widget = new MainAreaWidget<HeatmapWidget>({ content });
+          widget = new WebDSWidget<HeatmapWidget>({ content });
           widget.id = "webds_heatmap_widget";
           widget.title.label = "ADC Data";
           widget.title.icon = heatmapIcon;
@@ -62,7 +62,7 @@ const plugin: JupyterFrontEndPlugin<void> = {
       category: "WebDS - Exploration"
     });
 
-    let tracker = new WidgetTracker<MainAreaWidget>({
+    let tracker = new WidgetTracker<WebDSWidget>({
       namespace: "webds_heatmap"
     });
     restorer.restore(tracker, { command, name: () => "webds_heatmap" });
