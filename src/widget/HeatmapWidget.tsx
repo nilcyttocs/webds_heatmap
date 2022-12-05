c84b2b308703f9589c3e5a780a69321d6c65e292
Scott Lin <scott.lin@tw.synaptics.com>
2022-12-05 13:00:53 +0800

    Consolidate div elements for widget styling

--- src/widget/HeatmapWidget.tsx
+++ src/widget/HeatmapWidget.tsx
@@ -18,12 +18,8 @@ export class HeatmapWidget extends ReactWidget {
 
   render(): JSX.Element {
     return (
-      <div id={this.id + "_container"} className="jp-webds-widget-container">
-        <div id={this.id + "_content"} className="jp-webds-widget">
-          <HeatmapComponent service={this.service} />
-        </div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-top"></div>
-        <div className="jp-webds-widget-shadow jp-webds-widget-shadow-bottom"></div>
+      <div id={this.id + "_component"}>
+        <HeatmapComponent service={this.service} />
       </div>
     );
   }
